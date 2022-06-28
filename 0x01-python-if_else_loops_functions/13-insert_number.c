a780aa956e04b08699209d069777487a5092aad2
nosakhare <nosakhareaghedo42@gmail.com>
2022-06-28 22:57:18 +0000

    added function to put a node in a sorted list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,34 @@
+/**
+ * insert_node - function to add a node in a sorted linked
+ * in ascending other
+ * @head: pointer to the first node of list
+ * @number: number to be added
+ * Return: pointer to new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tmp1 = malloc(sizeof(listint_t));
+	listint_t *tmp2 = *head;
+
+	if (tmp1 == NULL)
+	{
+		free(tmp1);
+		return (NULL);
+	}
+	tmp1->n = number;
+	tmp1->next = *head;
+	while ((tmp1->n != tmp1->next->n || tmp1->n < tmp1->next->n) &&
+			tmp1->next->next->n < tmp1->n)
+	{
+		if (tmp1->next == NULL)
+		{
+			tmp2->next = tmp1;
+			return (tmp1);
+		}
+		tmp1->next = tmp1->next->next;
+		tmp2 = tmp2->next;
+	}
+	tmp1->next = tmp1->next->next;
+	tmp2->next = tmp1;
+	return (tmp1);
+}
