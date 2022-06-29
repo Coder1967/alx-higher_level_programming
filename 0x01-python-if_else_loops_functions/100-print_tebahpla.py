57bbce34c37fedb88020bc3e6f5205f601043c0c
nosakhare <nosakhareaghedo42@gmail.com>
2022-06-29 00:16:42 +0000

    added function to put a node in a sorted list

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,4 @@
 i = 0
 for ch in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(ch - i)), end="")
-    if i == 0:
-        i = 32
-    else:
-        i = 0
+    i = 32 if i == 0 else 0
