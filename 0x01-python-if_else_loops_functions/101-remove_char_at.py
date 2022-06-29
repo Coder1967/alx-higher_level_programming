f2ebb86825c8719768ead3bf239adc64d84840b0
nosakhare <nosakhareaghedo42@gmail.com>
2022-06-29 00:19:08 +0000

    added function to put a node in a sorted list

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    st = ""
-    i = 0
-    l = len(str) - 1
-
-    while i < l:
-        if i == n:
-            continue
-        else:
-            st += str[i]
-        i += 1
-    return (st)
-
-
-print(remove_char_at("Best School", 3)) 
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0)) 
-print(remove_char_at("School", 10)) 
-print(remove_char_at("Python", -2))
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
