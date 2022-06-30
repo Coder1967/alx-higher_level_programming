754b3a212a3b5cb62edbb3d353b487c670a786c9
nosakhare <nosakhareaghedo42@gmail.com>
2022-06-30 12:13:59 +0000

    completed more tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-l = len(argv) - 1
-i = 1
+    l = len(argv) - 1
+    i = 1
 
-if l == 0:
-    print("0 arguments.")
-elif l == 1:
-    print("1 argument:")
-    print("{}: {}".format(l, argv[1]))
-else:
-    print("{} arguments:".format(l))
-    while i <= l:
-        print("{}: {}".format(i, argv[i]))
-        i += 1
+    if l == 0:
+        print("0 arguments.")
+    elif l == 1:
+        print("1 argument:")
+        print("{}: {}".format(l, argv[1]))
+    else:
+        print("{} arguments:".format(l))
+        while i <= l:
+            print("{}: {}".format(i, argv[i]))
+            i += 1
