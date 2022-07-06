3c528a87324f5a65b0abd3c4262cd6db2c802e56
nosakhare <nosakhareaghedo42@gmail.com>
2022-07-06 21:22:19 +0000

    adding another function

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,20 +3,21 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
-    s = roman_string.copy()
     my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
             "D": 500, "M": 1000}
     i = 0
     sum = 0
-    while i < len(s):
-        sum += my_dict.get(s[i])
-        if len(s)-1 != i:
-            if s[i] == "I" and (s[i+1] == "V" or s[i + 1] == "X"):
+    while i < len(roman_string):
+        sum += my_dict.get(roman_string[i])
+        if len(roman_string)-1 != i:
+            if roman_string[i] == "I" and (roman_string[i+1] == "V"
+                    or roman_string[i + 1] == "X"):
                 sum -= 2
-            elif s[i] == "X" and (s[i+1] == "C" or s[i+1] == "D" or s[i+1] == "L"
+            elif roman_string[i] == "X" and (roman_string[i+1] == "C" or
+                    roman_string[i+1] == "D" or roman_string[i+1] == "L"
                     or roman_string[i+1] == "M"):
                 sum -= 20
-            elif s[i] == "C" and (roman_string[i+1] == "D" or s[i+1] == "M"):
+            elif roman_string[i] == "C" and (roman_string[i+1] == "D" or roman_string[i+1] == "M"):
                 sum -= 200
 
         i += 1
