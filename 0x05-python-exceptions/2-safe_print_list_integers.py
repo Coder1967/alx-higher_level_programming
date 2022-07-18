2a38531fc90d98e0e5727e2a8fe8087413aee682
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-07-18 12:59:31 +0000

    added another function

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,9 +6,10 @@ def safe_print_list_integers(my_list=[], x=0):
 
     while i < x:
         try:
-            print("{:d}".format(my_list[i], end=""))
+            print("{:d}".format(my_list[i]), end="")
             num_printed += 1
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             pass
         i += 1
+    print()
     return num_printed
