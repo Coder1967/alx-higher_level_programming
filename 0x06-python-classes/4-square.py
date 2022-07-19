571363574bfce9f0d324a574e990cf9e3f68fa18
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-07-19 20:49:31 +0000

    creating a class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" creating a class """
+
+
+class Square:
+    """ initializung a squar """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+
+    def area(self):
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        return (self.__size * self.__size)
