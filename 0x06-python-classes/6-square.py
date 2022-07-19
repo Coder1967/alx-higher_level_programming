f993e772b165bf90f4971aba4fd775dd5567d964
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-07-19 22:11:07 +0000

    creating a class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,51 +1,63 @@
 #!/usr/bin/python3
 
-""" creating a class """
+""" defines class Square """
 
 
 class Square:
-    """ initializung a squar """
-    def __init__(self, size=0, position= (0, 0)):
-        if type(size) != int:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        self.__position = position
+    """ defines the class square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """initializes a Square with the size and position"""
+
+        self.size = size
+        self.position = position
+
+    def area(self):
+
+        return (self.__size ** 2)
+
+    def my_print(self):
+
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for j in range(self.__size):
+                print(" " * self.position[0], end='')
+                print("#" * self.__size)
 
     @property
     def size(self):
-        return self.__size
+        """retrieves the private attribute size"""
+
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+        """Sets the size private attribute"""
+
+        if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
 
     @property
     def position(self):
-        return self.__position
+        """retrieves  the Position"""
+
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple or len(value) != 2
-        or tuple[0] < 0 or tuple[1] < 0:
+        """Sets the position"""
+
+        if (len(value) != 2) or (type(value) is not tuple) \
+                or (type(value[0]) is not int) \
+                or (type(value[1]) is not int) \
+                or (value[0] < 0) or (value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
-    def area(self):
-        return self.__size * self.__size
-
-    def my_print(self):
-        i = 0
-
-        if self.__size == 0:
-            print()
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        else:
+            self.__position = value
