7e8a360282b09458cb6ae804faf581987ac080b7
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-07-25 12:47:43 +0000

    added more modules for class project

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,16 +6,16 @@ class Rectangle:
     """ initializing function """
     def __init__(self, width=0, height=0):
          if type(width) != int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
+             raise TypeError("width must be an integer")
+         if width < 0:
+             raise ValueError("width must be >= 0")
+         if type(height) != int:
+             raise TypeError("height must be an integer")
+         if height < 0:
+             raise ValueError("height must be >= 0")
 
-        self.__width = width
-        self.__height = height
+         self.__width = width
+         self.__height = height
 
     """ getter and setter for width """
     @property
