adf7817d01cb679a10fed1ef2ef042b782213063
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-07-26 19:54:54 +0000

    added a class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" creation of a class """
+
+
+class LockedClass:
+    """ saving space by assigning memory for only 'first_name
+    using '__slot__'"""
+    __slot__[first_name]
