3f22152c7f5a989e91e7b878ca10f134b53c320b
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-08-01 12:54:34 +0000

    added another fuctionality

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,20 +6,19 @@
 class MyList(list):
     """ creating a function that sorts the list using bubble sort"""
     def print_sorted(self):
-        sorted_list = list(map(lambda x: x, self))
         i, j = 0, 0
         check, tmp = 0, 0
 
         while (i < len(self)):
             check = 0
             while j < len(self) - 1:
-                if sorted_list[j] > sorted_list[j + 1]:
-                    tmp = sorted_list[j + 1]
-                    sorted_list[j + 1] = sorted_list[j]
-                    sorted_list[j] = tmp
+                if self[j] > self[j + 1]:
+                    tmp = self[j + 1]
+                    self[j + 1] = self[j]
+                    self[j] = tmp
                     check = 1
                 j += 1
             if check == 0:
                 break
             i += 1
-        print(list(sorted_list))
+        print(list(self))
