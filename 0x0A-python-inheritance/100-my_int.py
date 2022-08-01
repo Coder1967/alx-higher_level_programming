202a2510a951da624c20f161ceecfc404a9da835
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-08-01 22:29:06 +0000

    created the added more functionality to the BaseGeomrtry class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ initializing a 'MyInt' class """
 
+
 class MyInt(int):
+    """ MyInt inherits from the int class """
     def __eq__(self, value):
         if (self - value == 0):
             return False
