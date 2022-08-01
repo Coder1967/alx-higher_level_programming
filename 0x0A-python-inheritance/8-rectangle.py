b709253345549a79743073ca7b82e8ac2c30a088
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-08-01 21:37:37 +0000

    created the added more functionality to the BaseGeomrtry class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
-""" creating BaseGeometry class """
-
-
-class BaseGeometry:
-    """ creating a method 'area' """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ checks if 'value' is a positive integer """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-""" defining 'Rectangle' class """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+""" defining 'Rectangle' class that inherits from BaseGeometry"""
 
 
 class Rectangle(BaseGeometry):
