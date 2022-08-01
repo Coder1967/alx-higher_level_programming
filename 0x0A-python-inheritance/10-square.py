6a42c36f0f4933018265a0af053b12af7eecccca
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-08-01 22:03:49 +0000

    created the added more functionality to the BaseGeomrtry class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -40,5 +40,5 @@ class Square(Rectangle):
     """ initializing class """
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size)
+        self.__size = size
