2df6c8af4cc7a111607b82d35626f345d8a8e88a
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-08-02 14:41:48 +0000

    class to dict function added

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -10,11 +10,4 @@ def class_to_json(obj):
     returns a dict of the object and
     its attribute
     """
-    my_dict = {}
-    for content in dir(obj):
-        if content[0] != '_' and content[1] != '_':
-            attr = "obj"
-            attr += "."
-            attr += content
-            my_dict[content] = eval(attr)
-    return my_dict
+    return obj.__dict__
