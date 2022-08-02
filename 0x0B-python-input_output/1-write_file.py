0c15135390e7576e3920056d889e5554d683edb3
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-08-02 12:03:18 +0000

    adding a function to write to a txt file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,4 +8,4 @@ def write_file(filename="", text=""):
     opening the file in the write mode
     """
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
+        return(f.write(text))
