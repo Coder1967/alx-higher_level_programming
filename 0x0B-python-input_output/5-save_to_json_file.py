bbf4b17ebfc4c545b431aa3abc1cdbc13d344035
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-08-02 12:46:47 +0000

    adding a function to convert a python object to a json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function converts python object into
+json and stores it in a file
+"""
+from json import dump
+
+def save_to_json_file(my_obj, filename):
+    """
+    opening file in write mode
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        dump(my_obj, f)
