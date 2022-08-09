a5acb89b3fef4be19dd7df692ed51b7830d500b8
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-08-09 06:54:20 +0000

    corrections

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ passing to the super class """
-        Rectangle.__init__(self, size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
         self.size = size
 
     """getter for size"""
