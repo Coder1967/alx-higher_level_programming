bd416768313688b3a824dbc84241f10a906552c2
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-09-07 15:40:11 +0100

    completed more tasks

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,3 @@
 -- a script that creates the table force_name on your MySQL server.
---name can’t be null and id is an int
--- the table force_name already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
+
