5f32e7ef8a42cc6ecedd2e20def4ec4be46d544f
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-09-07 17:58:33 +0100

    completed more tasks

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa
 SELECT c.id, c.name, s.name
 FROM cities AS c
-INNER JOIN states as s
+LEFT JOIN states as s
 ON s.id = c.state_id
 ORDER BY c.id;
 
