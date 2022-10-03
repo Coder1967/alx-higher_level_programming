f0a1cb7ee5ef6a40a5d5ea6419ba2cbe725ad084
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-03 20:45:37 +0000

    finished up the tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,23 +2,26 @@
 const process = require('process');
 let [check, tmp] = [0, 0];
 const arr = [];
+if (process.argv.length === 3 || process.argv.length === 2) {
+  console.log(0);
+} else {
+  for (let i = 2; i < process.argv.length; i++) {
+    arr.push(parseInt(process.argv[i]));
+  }
 
-for (let i = 2; i < process.argv.length; i++) {
-  arr.push(parseInt(process.argv[i]));
-}
-
-while (true) {
-  check = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] <= arr[i + 1]) {
-      tmp = arr[i];
-      arr[i] = arr[i + 1];
-      arr[i + 1] = tmp;
-      check = 1;
+  while (true) {
+    check = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] <= arr[i + 1]) {
+        tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        check = 1;
+      }
+    }
+    if (check === 0) {
+      break;
     }
   }
-  if (check === 0) {
-    break;
-  }
+  console.log(arr[1]);
 }
-console.log(arr[1]);
