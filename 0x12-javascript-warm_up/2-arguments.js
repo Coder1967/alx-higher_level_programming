10dfdb21d2362fd420624f98cf0f8263819be82b
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-03 19:17:55 +0000

    finished up more tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const process = require('process');
 
 if (process.argv.length > 2) {
   console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
