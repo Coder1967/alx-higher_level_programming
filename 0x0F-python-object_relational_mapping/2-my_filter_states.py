33fc8ef96c34b8583258f6249b80ea955e621daf
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-07 20:29:19 +0000

    completed another task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,11 +6,11 @@ using the database hbtn_0e_0_usa
 from sys import argv
 import MySQLdb
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
+    db = MySQLdb.connect(user=argv[1],
                          db=argv[3], port=3306, host='localhost')
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE states.name = %s
-                ORDER BY states.id ASC;""", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'"
+                .format(argv[4]))
     results = cur.fetchall()
     for res in results:
         print(res)
