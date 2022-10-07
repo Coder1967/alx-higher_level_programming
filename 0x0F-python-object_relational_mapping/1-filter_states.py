9ac26397e0237b5c05daebc7543b1c19d8a2bf0a
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-07 19:48:35 +0000

    completed another task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
 connecting with a database and making a simple query
+using the database hbtn_0e_0_usa
 """
-from sys import argv
-import MySQLdb
 if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
     db = MySQLdb.connect(user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306, host='localhost')
     cur = db.cursor()
