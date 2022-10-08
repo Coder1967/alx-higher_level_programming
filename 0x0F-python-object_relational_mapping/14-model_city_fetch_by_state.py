29b47be19bc0dc38d01313f6b6d34d77c5452cf6
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-08 22:36:10 +0000

    completed another task

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module updates  state with id of 2
+   to 'New Mexico'
+"""
+from sys import argv
+from model_state import State, Base
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    i = 0
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rows = session.query(State.name, City.id, City.name).join(City).order_by(City.id).all()
+    
+    for row in rows:
+        print('{}: ({}) {}'.format(row[0], row[1], row[2]))
+
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" creating table 'cities' in
+    the database
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Integer, Column, ForeignKey
+from model_state import State, Base
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer(), unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer(), ForeignKey('states.id'), nullable=False)
