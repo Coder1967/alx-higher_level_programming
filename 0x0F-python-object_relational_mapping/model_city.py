5954fdf4776398c2329968f983b2fe11354a63a0
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-08 22:40:14 +0000

    fixed some pycodestyle errors

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,6 +8,9 @@ from model_state import State, Base
 
 
 class City(Base):
+    """
+    definition for the class 'cities'
+    """
     __tablename__ = 'cities'
     id = Column(Integer(), unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
