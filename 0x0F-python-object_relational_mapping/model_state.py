e809a891100166ce5221ebd2eed36566fa66aae0
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-08 20:02:56 +0000

    copleted another task

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,11 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    class State creates a table 'states' in a
+    database
+    """
     __tablename__ = "states"
-    id = Column(Integer(), unique=True, autoincrement=True,
+    id = Column(Integer(), unique=True,
                 primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
