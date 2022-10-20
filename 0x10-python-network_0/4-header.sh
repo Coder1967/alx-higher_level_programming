fe950cbeee1b2d52fd7beade7afe64dab3390f0b
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-10-20 17:09:13 +0100

    completing another rask

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#sends a get method to the server with variabl 'X-School-User-Id' with value 98
-
-curl -sX GET -H "X-School-User-Id: 98" "$1"
+#sends a get method to the server with a variable
+curl -sX GET -H "X-School-User-Id: 98" $1
