2ce04f50e8114fee7120ea055a73d5c4478f97c1
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-21 11:44:43 +0000

    finishing up another task

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the response (decoded in utf-8)
+"""
+from sys import argv
+import urllib
+if __name__ == '__main__':
+    params = {email: argv[2]}
+    value = urllib.parse.urlencode(params)
+    url = urllib.request.Request(argv[1], value)
+    with urllib.request.urlopen(url) as res:
+        print(res.read())
