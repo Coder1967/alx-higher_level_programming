b221cb9293beb4b75177b9175dbe3bf0195c4091
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-10-21 13:48:50 +0000

    finishing up mandatory tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     import requests
     from sys import argv
     r = requests.get('https://api.github.com/user',
-            auth=(argv[1], argv[2]))
+                     auth=(argv[1], argv[2]))
     try:
         print(r.json()['id'])
     except KeyError:
