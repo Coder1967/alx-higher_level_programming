fe80725e7d8048802057c488531e1167e6af2701
Coder1967 <nosakhareaghedo42@gmail.com>
2022-11-15 21:19:09 +0000

    finishing up another task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/*
+ * Write a script that computes the number of tasks completed by user id.
+ * 1. The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+ * 2. Only print users and number of completed task
+ * 3. You must use the module request
+ */
+const request = require('request');
+const url1 = process.argv[2] + '?completed=true';
+let data = [];
+const myObj = {};
+let i = 1;
+let currentUser = 1;
+
+request(url1, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  data = JSON.parse(body);
+  data.forEach(function (value) {
+    if (currentUser !== value.userId) {
+      i = 1;
+      currentUser += 1;
+    }
+    myObj[value.userId] = i;
+    i += 1;
+  });
+  console.log(myObj);
+}
+);
