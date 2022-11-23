5ba4ecfaf2640e0b184b3e09ef108817e0b21e4a
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-11-23 21:31:08 +0100

    finishing first task

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/*
+ * JavaScript script that updates the text color of the <header> element to red (#FF0000)
+ */
+const tag = document.querySelector("header");
+tag.style.color = 'red';
